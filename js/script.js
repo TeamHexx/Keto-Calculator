9e92f910df624fbac99e0c3f0e732b90b655912d
hexx <hmrodrigueziii@gmail.com>
2021-03-22 16:53:26 -0700

    feat: adds search and clear

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+(function () {
 // constant variables - data that never changes
 const $input = $('input[type="text"]');
 // const $brand_name = $('#brand_name');
@@ -12,13 +13,14 @@ const $brand_name = $('#brand_name');
 // functions - code that represents actions taken/carried out
 // init();
 // function init() {
-//     handlegetData()
+//     handlegetData();
 // }
+    
 function handlegetData(event) {
     event.preventDefault();
     const userInput = $input.val()
     console.log('userInput', userInput)
-    $.ajax({ url: 'https://api.nutritionix.com/v1_1/search/'+userInput+'?results=0:20&fields=item_name,brand_name,nf_sugars,nf_dietary_fiber,nf_total_carbohydrate,nf_calories&appId=4e5eb041&appKey=03ce9db54e9af884189a9857ae7b2592'
+    $.ajax({ url: 'https://api.nutritionix.com/v1_1/search/'+userInput+'?results=0:40&fields=item_name,brand_name,nf_sugars,nf_dietary_fiber,nf_total_carbohydrate,nf_calories&appId=4e5eb041&appKey=03ce9db54e9af884189a9857ae7b2592'
 }).then(function (data) {
     hits = data;
     render();
@@ -27,17 +29,23 @@ function handlegetData(event) {
 });
 }
 
-function render() {``
+function render() {
+    console.log(hits);
+    $hits.empty();
+    console.log("im not working");
     const html = hits.hits.map(function(hit) {
-        return `
+            return `
             <article class="card">
-                <h1>${hit.fields.item_name}</h1>
-                <p>${hit.fields.nf_sugars + ' grams of sugar'}</p>
-                <p>${hit.fields.nf_total_carbohydrate + ' grams of carbs'}</p>
-                <p>${hit.fields.nf_dietary_fiber + ' grams of fiber'}</p>
-                <p>${hit.fields.nf_calories + ' calories'}</p>
+                <h2>${hit.fields.item_name}</h2>
+               <ul>
+                <li><strong style="font-size: 18px">${hit.fields.nf_sugars}</strong> grams of sugar</li>
+                <li><strong style="font-size: 18px">${hit.fields.nf_total_carbohydrate}</strong> grams of carbs</li>
+                <li><strong style="font-size: 18px">${hit.fields.nf_dietary_fiber}</strong> grams of fiber</li>
+                <li><strong style="font-size: 18px">${hit.fields.nf_calories}</strong> calories</li></ul>
             </article>
         `
+        
     });
     $hits.append(html);
 }
+})();
